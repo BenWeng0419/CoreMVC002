08822d1d58a5d0c265b78bc142ed3649839aef76
USER <USER@ELVA220922-1>
2024-10-08 15:01:51 +0800

    加入專案檔案。

---
+++ CoreMVC002/Controllers/GameController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoreMVC002.Models;
+
+namespace CoreMVC002.Controllers
+{
+    public class GameController : Controller
+    {
+        private static XAXBEngine _game;
+
+        public IActionResult Index()
+        {
+            if (_game == null)
+            {
+                _game = new XAXBEngine();
+            }
+            return View(_game);
+        }
+
+        [HttpPost]
+        public IActionResult Guess(string guessNumber)
+        {
+            if (_game.IsGameOver(guessNumber))
+            {
+                ViewBag.GameOver = true;
+                ViewBag.Message = "恭喜！你已經猜到這個數字了！";
+            }
+            else
+            {
+                ViewBag.GameOver = false;
+            }
+            return View("Index", _game);
+        }
+
+        [HttpPost]
+        public IActionResult Restart()
+        {
+            _game.Reset();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CoreMVC002/Models/XAXBEngine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreMVC002.Models
+{
+    public class XAXBEngine
+    {
+        public string Secret { get; private set; }
+        public string Guess { get; private set; }
+        public string Result { get; private set; }
+        public List<GuessResult> GuessHistory { get; private set; }
+        public int AttemptCount { get; private set; }
+
+        public XAXBEngine()
+        {
+            Random random = new Random();
+            Secret = random.Next(1000, 10000).ToString(); // Generates a random 4-digit number
+            Guess = null;
+            Result = null;
+            GuessHistory = new List<GuessResult>();
+            AttemptCount = 0;
+        }
+
+        public XAXBEngine(string secretNumber)
+        {
+            Secret = secretNumber;
+            Guess = null;
+            Result = null;
+            GuessHistory = new List<GuessResult>();
+            AttemptCount = 0;
+        }
+
+        public int NumOfA(string guessNumber)
+        {
+            return guessNumber.Zip(Secret, (g, s) => g == s).Count(match => match);
+        }
+
+        public int NumOfB(string guessNumber)
+        {
+            return guessNumber.Count(g => Secret.Contains(g)) - NumOfA(guessNumber);
+        }
+
+        public bool IsGameOver(string guessNumber)
+        {
+            AttemptCount++;
+            Guess = guessNumber;
+            int aCount = NumOfA(guessNumber);
+            int bCount = NumOfB(guessNumber);
+            Result = $"{aCount}A{bCount}B";
+            GuessHistory.Add(new GuessResult { Guess = guessNumber, Result = Result });
+
+            return aCount == 4; // Game over if all 4 digits are correct
+        }
+
+        public void Reset()
+        {
+            Random random = new Random();
+            Secret = random.Next(1000, 10000).ToString(); // Resets with a new random number
+            GuessHistory.Clear();
+            AttemptCount = 0;
+            Guess = null;
+            Result = null;
+        }
+    }
+
+    public class GuessResult
+    {
+        public string Guess { get; set; }
+        public string Result { get; set; }
+    }
+}
